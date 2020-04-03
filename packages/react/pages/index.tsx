e0a5d5b73c0daf76ec573294fcd0efd990446c04
Andreas Adam <andreas.sahle@gmail.com>
2020-04-03 05:52:26 +0200

    chore(setup): base setup and nextjs scaffold

---
+++ packages/react/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const Home = () => (
+  <div className="container">
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main>
+      <h1 className="title">
+        Welcome to the better/best Social Network!!
+      </h1>
+    </main>
+
+  </div>
+)
+
+export default Home
---
+++ packages/react/prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  printWidth: 119,
+  singleQuote: true
+};
